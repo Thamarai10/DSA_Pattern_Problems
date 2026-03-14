e39598412e7efeb2d4935111de4125287f3fe210
THAMARAI B <thamaraib102@gmail.com>
2026-03-14 18:57:10 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,34 @@
+/*
+n = 4
+
+*
+* * 
+* * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
+import java.util.*;
+public class pattern10
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value: ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		for(int i=n-1;i>=1;i--){
+		    for(int j=1;j<=i;j++){
+		        System.out.print("* ");
+	    	}
+	    	System.out.println();
+		}
+		
+	}
+}
---
+++ pattern9.java
@@ -0,0 +1,40 @@
+/*
+n = 4
+
+      *
+    * * *
+  * * * * * 
+* * * * * * *
+* * * * * * *
+  * * * * *
+    * * *
+      *
+*/
+import java.util.*;
+public class pattern9
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value: ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=n-i;j++){
+		        System.out.print("  ");
+		    }
+		    for(int k=1;k<=(2*i-1);k++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		for(int i=n;i>=1;i--){
+		    for(int j=1;j<=(n-i);j++){
+		        System.out.print("  ");
+		    }
+		    for(int k=1;k<=(2*i-1);k++){
+		    System.out.print("* ");
+	    	}
+	    	System.out.println();
+		}
+		
+	}
+}
